a3c5eef47bf48ed8ecd8c2c29ec0b4ca725108c1
Timofey <48101066+Mita57@users.noreply.github.com>
2019-10-12 18:50:26 +0500

    Continuing working on classes

--- app.py
+++ app.py
@@ -7,6 +7,5 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello Tenzor!'
 
-
 if __name__ == '__main__':
     app.run(debug=True)
--- models/users.py
+++ models/users.py
@@ -1,8 +1,29 @@
 import datetime
+import sqlite3
 
 
-#abstract af
-class user:
+class BasicModel:
+    _FIELDS_MAPPING = {}
+
+    def __getattr__(self, item):
+        if item in self._FIELDS_MAPPING.keys():
+            return self._FIELDS_MAPPING[item]
+        raise AttributeError
+
+    def __setattr__(self, key, value):
+        if key in self._FIELDS_MAPPING.keys():
+            self._FIELDS_MAPPING[key] = value
+        raise AttributeError
+
+
+class User(BasicModel):
+    _FIELDS_MAPPING = {
+        'nickname': str,
+        'join_date': datetime,
+        'password': str,
+        'email': str,
+        'birth_date': datetime,
+    }
 
     def login(self, password, nickname):
         pass
@@ -16,8 +37,7 @@ class user:
         self.password = password
 
 
-
-class moderator(user):
+class Moderator(user):
     def __init__(self, nickname, join_date, password, service_count):
         self.nickname = nickname
         self.join_date = join_date
@@ -27,9 +47,11 @@ class moderator(user):
     def delete_chat_message(self):
         pass
 
-    def ban (self, nickname):
+    @staticmethod
+    def ban(self, nickname):
         users[nickname].banned = True
 
+    @staticmethod
     def unban(self, nickname):
         users[nickname].banned = False
 
