774ec61efa5c428c1346b7758e2da64854398ca0
Timofey <48101066+Mita57@users.noreply.github.com>
2019-10-22 10:27:31 +0500

    Design stuff

---
+++ designStuff/js/script.js
@@ -0,0 +1,41 @@
+function gamingOn() {
+    document.getElementById("gamingPic").src = "assets/gamingHubHover.png"
+}
+function gamingOut() {
+    document.getElementById("gamingPic").src = "assets/gamingHub.png"
+}
+
+function creativeOn() {
+    document.getElementById("creativePic").src = "assets/creativeHubHover.png"
+}
+function creativeOut() {
+    document.getElementById("creativePic").src = "assets/creativeHub.png"
+}
+
+function podcastsOn() {
+    document.getElementById("podcastPic").src = "assets/podcastsHubHover.png"
+}
+function podcastsOut() {
+    document.getElementById("podcastPic").src = "assets/podcastsHub.png"
+}
+
+function sciOn() {
+    document.getElementById("sciPic").src = "assets/sciHubHover.png"
+}
+function sciOut() {
+    document.getElementById("sciPic").src = "assets/sciHub.png"
+}
+
+function irlOn() {
+    document.getElementById("irlPic").src = "assets/irlHubHover.png"
+}
+function irlOut() {
+    document.getElementById("irlPic").src = "assets/irlHub.png"
+}
+
+function etcOn() {
+    document.getElementById("etcPic").src = "assets/etcHubHover.png"
+}
+function etcOut() {
+    document.getElementById("etcPic").src = "assets/etcHub.png"
+}
