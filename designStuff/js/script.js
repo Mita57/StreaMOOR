3d4979bf88a211269aa31ca3ddbafe0ef69a8458
Timofey <48101066+Mita57@users.noreply.github.com>
2019-10-22 12:58:05 +0500

    all the channels

--- designStuff/js/script.js
+++ designStuff/js/script.js
@@ -39,3 +39,14 @@ function etcOn() {
 function etcOut() {
     document.getElementById("etcPic").src = "assets/etcHub.png"
 }
+function chatMock(){
+    var i = 0;
+    vars = ['<li class="list-group-item"><p><span class="chat_user">outism:</span>А А АА А А АА А А А ААА</p></li>',
+        '<li class="list-group-item"><p><span class="chat_user">RemoveKebab:</span>А А АА А А АА А А А ААА</p></li>',
+        '<li class="list-group-item"><p><span class="chat_user">CheckMother:</span>А А АА А А АА А А А ААА</p></li>',
+        '<li class="list-group-item"><p><span class="chat_user">SIAKOD:</span>ЛУЛУУЛУЛУЛУЛ АА</p></li>'];
+    setInterval(function(){
+        document.getElementById("chattik").innerHTML+=vars[i%4];
+        i++;
+    },1000)
+}
