3c4479fa56f81558f43725ad2aed5f4be4da4982
Timofey <48101066+Mita57@users.noreply.github.com>
2019-11-18 07:15:33 +0500

    o

--- client/src/router.js
+++ client/src/router.js
@@ -1,11 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
-import Tasks from '@/views/Tasks';
-import Login from '@/views/Login';
-import SignUp from '@/views/SignUp';
-import Account from'@/views/Account';
-import CreateTask from "@/views/CreateTask";
 import Info from "@/views/Info";
 
 Vue.use(Router);
@@ -14,34 +9,8 @@ const router = new Router({
   routes: [
     {
       path: '*',
-      redirect: '/login'
+      redirect: '/info'
     },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login
-    },
-    {
-      path: '/sign-up',
-      name: 'SignUp',
-      component: SignUp
-    },
-      {
-          path: '/tasks',
-          name: 'Tasks',
-          component: Tasks,
-      },
-    {
-      path:'/account',
-      name:'Account',
-      component: Account,
-    },
-    {
-      path:'/createTask',
-      name:'Create task',
-      component: CreateTask,
-      }
-      ,
     {
       path:'/info',
       name:'Info',
