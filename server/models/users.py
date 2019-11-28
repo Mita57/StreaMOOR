bda80b15946254952615b0e638aed8dc766f320f
Timofey <48101066+Mita57@users.noreply.github.com>
2019-11-28 08:33:41 +0500

    removed useless elements from teh models

--- server/models/users.py
+++ server/models/users.py
@@ -10,6 +10,18 @@ class User(BasicModel):
     _PASSWORD = 'MOORMOOR'
     _HOST = '127.0.0.1'
 
+    nickname: str
+    join_date: datetime
+    password: str
+    img: str
+    email: str
+    description: str
+    birth_date: datetime
+    status: str
+    banned: bool,
+
+
+
 
     _FIELDS_MAPPING = {
         'nickname': str,  # primary key
@@ -20,9 +32,6 @@ class User(BasicModel):
         'description': str,
         'birth_date': datetime,
         'status': str,
-        'banned': bool,
-        'online': bool,
-        'current_hub': str,
     }
 
     def login(self, password, nickname):
@@ -31,16 +40,13 @@ class User(BasicModel):
     def logout(self, nickname):
         pass
 
-    def __init__(self, nickname, join_date, password, email, birth_date, watching_now):
+    def __init__(self, nickname, join_date, password, email, birth_date):
         self.nickname = nickname
         self.join_date = join_date
         self.password = password
         self.email = email
         self.status = type(self).__name__
         self.birth_date = birth_date
-        self.banned = False
-        self.online = True
-        self.watching_now = watching_now
 
 
 class Moderator(User):
@@ -54,14 +60,10 @@ class Moderator(User):
         'description': str,
         'status': str,
         'service_count': int,
-        'banned': bool,
-        'online': bool,
-        'watching_now': int,
-        'current_hub': str,
     }
 
-    def __init__(self, nickname, join_date, password, service_count, email, birth_date, watching_now):
-        super().__init__(nickname, join_date, password, email, birth_date, watching_now)
+    def __init__(self, nickname, join_date, password, service_count, email, birth_date):
+        super().__init__(nickname, join_date, password, email, birth_date)
         self.service_count = service_count
 
     @staticmethod
