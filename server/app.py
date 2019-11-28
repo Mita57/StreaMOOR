306425ef47a7d0f31e15ebde32f5a60d7063a090
Timofey <48101066+Mita57@users.noreply.github.com>
2019-11-28 16:30:36 +0500

    added async stuff

--- server/app.py
+++ server/app.py
@@ -1,8 +1,8 @@
-import json
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from .models import SQLModel
 import datetime
+import asyncio
 
 app = Flask(__name__)
 
@@ -13,13 +13,13 @@ def index():
 
 
 @app.route('/login', methods=['POST'])
-def login():
+async def login():
     """
     Gets the data from the login form and authorizes the input
 
     Returns: JSON with auth result info
     """
-    post_data = request.get_json()
+    post_data = await request.get_json()
     user = SQLModel.get_by_attrs('email, password', 'email', (post_data.get('email')))
     if post_data.get('pwrd') == user[0][1]:
         return jsonify(style='color:lightgreen', info='Вход выполнен', flag=True)
@@ -28,20 +28,21 @@ def login():
 
 
 @app.route('/register', methods=['POST'])
-def register():
+async def register():
     """
     Inserts the information about new user unto the database
 
     Returns:
         JSON with the result
     """
-    post_data = request.get_json()
+    post_data = await request.get_json()
     if SQLModel.get_by_attrs('email, password', 'email', (post_data.get('email'))):
         return jsonify(info='Данный пользователь уже зарегестрирован')
     else:
         date = datetime.date.day + '/' + datetime.date.month + '/' + datetime.date.year
         SQLModel.insert((post_data.get('nickname'), post_data.get('email'), post_data.get('password'), None,
                          date, None, post_data.get('birthDate'), 'user', False))
+        return jsonify(info='Регистрация прошла успешно')
 
 
 if __name__ == '__main__':
