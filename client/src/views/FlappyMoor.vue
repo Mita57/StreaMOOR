4482b2a4a4bf29ebe578d1d74fa6611542214f0f
Timofey <48101066+Mita57@users.noreply.github.com>
2019-12-06 10:14:22 +0500

    kostyl

--- client/src/views/FlappyMoor.vue
+++ client/src/views/FlappyMoor.vue
@@ -1,10 +1,19 @@
 <template>
-
+    <div id="sas">
+    </div>
 </template>
 
 <script>
-    export default {
-        name: "FlappyMoor"
+    export default{
+        name: "FlappyMoor",
+        mounted(){
+            let phaserScript = document.createElement('script');
+            phaserScript.setAttribute('src', '//cdn.jsdelivr.net/npm/phaser@3.21.0/dist/phaser.js');
+            document.head.appendChild(phaserScript);
+            let gameScript = document.createElement('script');
+            gameScript.setAttribute('src', '../../flappyMoor/js/script.js');
+            document.head.appendChild(gameScript);
+        },
     }
 </script>
 
